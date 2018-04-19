20c39510649a955a50b109f0900a5dcc8d5ab5bd
esteban1234 <tva_libra@hotmail.com>
2018-04-19 12:58:44 -0500

    metas

--- vistas/encabezado.php
+++ vistas/encabezado.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	
+	<meta name="google-site-verification" content="nc8MT2f2CuV3OAMTslI2wcWqx7-KOXFL7Jmh0rRNbMg" />
 	<link rel="icon" type="image/png" href="img/favicon.png" />
 	<title>PROVIT ASESORES</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
